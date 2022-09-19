cfe38a9878df4ba56a181e36fa7eff30ebd6b464
peewilliam <petryck@i9devgroup.com>
2022-09-19 15:30:12 -0300

    .
    .

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const ExpressApp = express();
+const http = require('http');
+const path = require('path');
+const formidable = require("formidable");
+const server = http.createServer(ExpressApp);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const fs = require("fs");
+const port = 2828;
+const sharp = require("sharp");
+
+ExpressApp.use(express.urlencoded({extended: true}));
+ExpressApp.use(express.json());
+ExpressApp.use('/', express.static(path.join(__dirname, 'arquivos')))
+
+async function compressImage(file, local) {
+ 
+    try {
+      const metadata = await sharp(file)
+      .toFormat('webp')
+      .webp({
+            quality: 80
+        })
+      .toFile(local);
+       fs.access(file, (err) => {
+        if (!err) {
+            fs.unlink(file, err_1 => {
+                if (err_1)
+                    console.log(err_1);
+            });
+        }
+    });
+    } catch (error) {
+      console.log(`An error occurred during processing: ${error}`);
+    }
+  }
+
+ExpressApp.post('/CadImgColaborador', async (req, res) => {
+    const form = new formidable.IncomingForm();
+    form.parse(req, async (err, fields, files) => {
+    //     console.log(fields)
+    // console.log(req.body)
+    console.log(files)
+    var extensaoArquivo;
+    var newpath;
+    var oldpath
+    extensaoArquivo = files.file.originalFilename.split('.')[1];
+      oldpath = files.file.filepath;
+  
+      newpath = path.join(__dirname, 'arquivos', Math.random()+'.'+extensaoArquivo);
+    
+  
+      fs.renameSync(oldpath, newpath);
+
+     
+    })
+
+})
+
+
+server.listen(port, () => {
+  
+    console.log(`Servidor backend executado: http://localhost:${port}`);
+    
+    
+  });
